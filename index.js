df7ad0610b40654aebbfd33064acbaf6cb1503da
JuliaShrestha <juliashrestha215@gmail.com>
2024-07-25 16:48:16 +0300

    Remove element

--- index.js
+++ index.js
@@ -12,11 +12,12 @@ console.log(array); // Результат: [1, 3, 6, 2, 5, 7]
 
 const array = [1, 3, 4, 6, 2, 5, 7];
 
-function removeElement () {
-    let removed = array.splice(2, 1);
+function removeElement (removeFromArray, numToDelete) {
+    
+    return removeFromArray.filter(function(number) {
+        return number !== numToDelete;
+    });
+};
 
-    return removed;
-}
-
-const result = removeElement();
-console.log(array);
+const result = removeElement(array, 7);
+console.log(result);
